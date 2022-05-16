08d09bc4c45795582cec0ec56603e3500a685bb4
david <f312213213david@gmail.com>
2022-05-16 19:26:27 +0800

    blurring image

--- components/CandidateCard/index.jsx
+++ components/CandidateCard/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
+import BlurImage from '@/components/CandidateCard/BlurImage'
 
 const CandidateCard = ({ candidate, path, routerParams, name }) => {
   return (
       <div className={'w-full h-96 rounded flex flex-col items-center shadow-lg bg-custom-200 dark:bg-custom-400 transition relative'}>
         <div className={'h-1/2 w-full relative'}>
-          <Image layout={'fill'} objectFit={'contain'} src={candidate.photoLink?.stringValue || candidate.photoURL?.stringValue} alt={candidate.introduction.stringValue} />
+          <BlurImage candidate={candidate} />
         </div>
         <div className={'h-1/2 w-full'}>
-
           <div className={'p-2 flex flex-col items-center'}>
             <div className={'font-bold text-xl'}>{candidate.username.stringValue}</div>
             <p className={'text-gray-700 text-base text-center'}>
